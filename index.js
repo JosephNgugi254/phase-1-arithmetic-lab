3bceb24bdaa829694ddf89b27ecf23192f0a53b0
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-03-09 11:20:10 +0300

    arithmetic vars

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let num3 = 10;
+let num4 = 6;
+let mod = 10 % 6;
+let max = Math.max(1,2,20,3,4);
